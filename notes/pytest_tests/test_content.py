336b7f24d1ff4d3d3d8f8f7d749c1a8b8d60a1ba
Andrew Kirov <AndrewKirov@me.com>
2023-07-21 12:04:24 +0500

    edit testing plan and unit/pytest content

---
+++ notes/pytest_tests/test_content.py
@@ -0,0 +1,56 @@
+import pytest
+from django.urls import reverse
+
+
+def test_note_in_list_for_author(note, author_client):
+    url = reverse('notes:list')
+    response = author_client.get(url)
+    object_list = response.context['object_list']
+    assert note in object_list
+
+
+def test_note_not_in_list_for_another_user(note, admin_client):
+    url = reverse('notes:list')
+    response = admin_client.get(url)
+    object_list = response.context['object_list']
+    assert note not in object_list
+
+
+@pytest.mark.parametrize(
+    'parametrized_client, note_in_list',
+    (
+        (pytest.lazy_fixture('author_client'), True),
+        (pytest.lazy_fixture('admin_client'), False),
+    )
+)
+def test_notes_list_for_different_users(
+        note, parametrized_client, note_in_list):
+    url = reverse('notes:list')
+    response = parametrized_client.get(url)
+    object_list = response.context['object_list']
+    assert (note in object_list) is note_in_list
+
+
+def test_create_note_page_contains_form(author_client):
+    url = reverse('notes:add')
+    response = author_client.get(url)
+    assert 'form' in response.context
+
+
+def test_edit_note_page_contains_form(slug_for_args, author_client):
+    url = reverse('notes:edit', args=slug_for_args)
+    response = author_client.get(url)
+    assert 'form' in response.context
+
+
+@pytest.mark.parametrize(
+    'name, args',
+    (
+        ('notes:add', None),
+        ('notes:edit', pytest.lazy_fixture('slug_for_args'))
+    )
+)
+def test_pages_contains_form(author_client, name, args):
+    url = reverse(name, args=args)
+    response = author_client.get(url)
+    assert 'form' in response.context
--- notes/tests/test_content.py
+++ notes/tests/test_content.py
@@ -11,7 +11,9 @@ class TestNotesCreateFormAvailible(TestCase):
     @classmethod
     def setUpTestData(cls) -> None:
         cls.add_url = reverse('notes:add')
+        cls.list_url = reverse('notes:list')
         cls.author = User.objects.create(username='test')
+        cls.reader = User.objects.create(username='Reader user')
         cls.note = Note.objects.create(
             title='Заголовок',
             text='Текст',
@@ -19,14 +21,34 @@ class TestNotesCreateFormAvailible(TestCase):
             slug='slug',
         )
 
-    def test_anonymous_client_has_no_form(self):
+    def test_anonymous_client_has_no_add_form(self):
         response = self.client.get(self.add_url)
         self.assertIsNotNone('form', response.context)
 
-    def test_authorized_client_has_form(self):
+    def test_authorized_client_has_add_form(self):
         self.client.force_login(self.author)
         response = self.client.get(self.add_url)
         self.assertIn('form', response.context)
+
+    def test_edit_note_page_contains_form(self):
+        self.edit_url = reverse('notes:edit', args=(self.note.slug,))
+        self.client.force_login(self.author)
+        response = self.client.get(self.edit_url)
+        self.assertIn('form', response.context)
+
+    def test_note_in_list_for_author(self):
+        self.client.force_login(self.author)
+        url = reverse('notes:list')
+        response = self.client.get(url)
+        object_list = response.context['object_list']
+        self.assertIn(self.note, object_list)
+
+    def test_note_not_in_list_for_another_user(self):
+        self.client.force_login(self.reader)
+        url = reverse('notes:list')
+        response = self.client.get(url)
+        object_list = response.context['object_list']
+        self.assertNotIn(self.note, object_list)
 
 
 class TestListPageOrder(TestCase):
@@ -35,7 +57,6 @@ class TestListPageOrder(TestCase):
     @classmethod
     def setUpTestData(cls) -> None:
         cls.author = User.objects.create(username='test')
-
         for index in range(5):
             notes = Note.objects.create(
                 title=f'{index}',
@@ -44,7 +65,7 @@ class TestListPageOrder(TestCase):
                 slug=f'{index}')
             notes.save()
 
-    def test_news_order(self):
+    def test_notes_order(self):
         self.client.force_login(self.author)
         response = self.client.get(self.LIST_URL)
         object_list = response.context['object_list']
